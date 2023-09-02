495da110fe1d056b887897218edce8d9fa35b604
souzale2 <plemos16@icloud.com>
2023-09-02 13:41:07 -0500

    Working Code Along Class 2

---
+++ CH01GuitarShop/GuitarShop/Controllers/ExampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
